492cbdd1078fffeed3c3a92d910adce96666d542
mileslucey <43838442+mileslucey@users.noreply.github.com>
2019-02-27 14:30:51 -0800

    update

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,6 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
+import pandas as pd
 
 
 def init_browser():
@@ -12,35 +13,79 @@ def init_browser():
 def scrape_info():
     browser = init_browser()
 
-    # PART ONE -- IMAGE OF MARS
+    # PART ONE -- LATEST MARS NEWS
+    # Visit the NASA website to find the top mars news article
+    mars_url = "https://mars.nasa.gov/news/"
+    browser.visit(mars_url)
+    time.sleep(1)
+    html_mars_site = browser.html
+    time.sleep(1)
+    # Scrape page into Soup
+    soup = bs(html_mars_site,"html.parser")
+    time.sleep(1)
+    # Find the the latest news title and headline text in soup
+    news_title = soup.find("div",class_="content_title").text
+    time.sleep(1)
+    news_p = soup.find("div",class_="article_teaser_body").text
+    time.sleep(1)
+
+    # PART TWO -- IMAGE OF MARS
     # Visit the NASA website to find the top mars image
     jpl_image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     jpl_image_url_base = "https://www.jpl.nasa.gov"
     browser.visit(jpl_image_url)
-    time.sleep(2)
+    time.sleep(1)
     browser.click_link_by_partial_text('FULL IMAGE')
-    time.sleep(2)
+    time.sleep(1)
     browser.click_link_by_partial_text('more info')
-    time.sleep(2)
+    time.sleep(1)
     # Scrape page into Soup
     html_image_site = browser.html
-    time.sleep(2)
+    time.sleep(1)
     mars_image_soup = bs(html_image_site,"html.parser")
     # Find the image in soup
     search_image = mars_image_soup.find(class_="main_image")
     featured_image_url = jpl_image_url_base + search_image["src"]
 
-    # PART TWO -- MARS WEATHER INFORMATION
+    # PART THREE -- MARS WEATHER INFORMATION
     # Visit the Twitter website to find the weather information
     tweet_weather_url = "https://twitter.com/marswxreport?lang=en"
     browser.visit(tweet_weather_url)
-    time.sleep(2)
+    time.sleep(1)
     # Scrape page into Soup
     html_weather_twitter = browser.html
-    time.sleep(2)
+    time.sleep(1)
     weather_soup = bs(html_weather_twitter,"html.parser")
     # Find the text in soup
     mars_weather = weather_soup.find("p",class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text
+
+    # PART 4 -- MARS FACTS TABLE
+    # Go to Space Facts website and pull a table of Mars facts
+    mars_facts_url = "https://space-facts.com/mars/"
+    browser.visit(mars_facts_url)
+    time.sleep(1)
+    mars_facts_html = browser.html
+    time.sleep(1)
+    mars_facts_soup = bs(mars_facts_html,"html.parser")
+    time.sleep(1)
+    mars_table = mars_facts_soup.find("table", class_ = 'tablepress tablepress-id-mars')
+    c1 = mars_table.find_all("td",class_="column-1")
+    c2 = mars_table.find_all("td",class_="column-2")
+
+    categories_mars = []
+    values_mars = []
+
+    for x in c1:
+        categories_mars.append(x.text.strip())
+    for y in c2:
+        values_mars.append(y.text.strip())
+    mars_facts_table = pd.DataFrame({
+        "Description": categories_mars,
+        "Value": values_mars 
+        })
+
+    mars_facts_table_html = mars_facts_table.to_html(header=False, index=False)
+
 
     # XXXXXXX
 
@@ -69,9 +114,11 @@ def scrape_info():
 
     # Store data in a dictionary
     mars_data = {
+        "news_title": news_title,
+        "news_p": news_p,
         "featured_image_url": featured_image_url,
         "mars_weather": mars_weather,
-        "max_temp": max_temp
+        "mars_facts_table_html": mars_facts_table_html
     }
 
     # Close the browser after scraping
